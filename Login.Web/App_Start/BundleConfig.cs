43661915dc1770bbccc45dc8624cb720b394bf24
Thiago de Melo Lima <Thiago_badboy300@hotmail.com>
2020-06-13 17:44:24 -0300

    Initial commit

---
+++ Login.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Login.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/login.css"));
+        }
+    }
+}
